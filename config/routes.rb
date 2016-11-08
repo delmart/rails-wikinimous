f026582d15dccd1a4986b559ef878a78d42262a8
Delphine Martinache <del.martinache@gmail.com>
2016-11-08 20:21:19 +0100

    exercice2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/create'
+
+  # get 'articles/update'
+
+  # get 'articles/destroy'
+
+  # get 'wikinimous/index'
+
+  # get 'wikinimous/show'
+
+  # get 'wikinimous/create'
+
+  # get 'wikinimous/update'
+
+  # get 'wikinimous/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
